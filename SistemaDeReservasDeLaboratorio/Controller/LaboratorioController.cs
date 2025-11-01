6cd625024ff47c28b67d260782be92ba00a12547
Emanuel Nadir Hamui <hamuiemanuel@gmail.com>
2025-11-01 19:31:51 -0300

    Controller hecho

--- SistemaDeReservasDeLaboratorio/Controller/LaboratorioController.cs
+++ SistemaDeReservasDeLaboratorio/Controller/LaboratorioController.cs
@@ -3,10 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SistemaDeReservasDeLaboratorio.Model;
 namespace SistemaDeReservasDeLaboratorio.Controller
 {
     internal class LaboratorioController
     {
+        private readonly Repository.ILaboratorioRepository _laboratorioRepository;
+        public LaboratorioController(Repository.ILaboratorioRepository laboratorioRepository)
+        {
+            _laboratorioRepository = laboratorioRepository;
+        }
+        public void AgregarLaboratorio(Laboratorio laboratorio)
+        {
+            if (laboratorio == null)
+            {
+                throw new ArgumentNullException(nameof(laboratorio), "El laboratorio no puede ser nulo");
+            }
+            _laboratorioRepository.Agregar(laboratorio);
+        }
+        public Laboratorio ObtenerLaboratoriosPorId(int id)
+        {
+            var Laboratorio = _laboratorioRepository.ObtenerPorId(id);
+            if (Laboratorio == null)
+            {
+                throw new KeyNotFoundException($"No se encontró un laboratorio con el ID {id}.");
+            }
+            return Laboratorio;
+        }
+
+        public void ActualizarLaboratorio(Laboratorio laboratorio)
+        {
+            if (laboratorio == null)
+            {
+                throw new ArgumentNullException(nameof(laboratorio), "El laboratorio no puede ser nulo");
+            }
+            _laboratorioRepository.Actualizar(laboratorio);
+        }
+        public IEnumerable<Laboratorio> ObtenerTodosLosLaboratorios()
+        {
+            return _laboratorioRepository.ObtenerTodos();
+        }
+
+        public void EliminarLaboratorio(int id)
+        {
+            var Laboratorio = _laboratorioRepository.ObtenerPorId(id);
+            if (Laboratorio == null)
+            {
+                throw new KeyNotFoundException($"No se encontró un laboratorio con el ID {id}.");
+            }
+            _laboratorioRepository.Eliminar(Laboratorio);
+        }
     }
 }
--- SistemaDeReservasDeLaboratorio/Controller/ReservaController.cs
+++ SistemaDeReservasDeLaboratorio/Controller/ReservaController.cs
@@ -3,12 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SistemaDeReservasDeLaboratorio.Model;
 namespace SistemaDeReservasDeLaboratorio.Controller
 {
     internal class ReservaController
     {
+        private readonly Repository.IReservaRepository _reservaRepository;
 
+        public ReservaController(Repository.IReservaRepository reservaRepository)
+        {
+            _reservaRepository = reservaRepository;
+        }
 
+        public void AgregarReserva(Reserva reserva)
+        {
+            if (reserva == null)
+            {
+                throw new ArgumentNullException(nameof(reserva), "la reserva no puede ser nula");
+            }
+            _reservaRepository.Agregar(reserva);
+        }
+        public IEnumerable<Reserva> ObtenerReservasPorFecha(DateTime fecha)
+        {
+            return _reservaRepository.ObtenerPorFecha(fecha);
+        }
+
+        public IEnumerable<Reserva> ObtenerReservasPorProfesor(string profesor)
+        {
+            if (string.IsNullOrWhiteSpace(profesor))
+                throw new ArgumentException("El nombre del profesor no puede estar vacío.", nameof(profesor));
+
+            return _reservaRepository.ObtenerPorProfesor(profesor);
+        }
+        public IEnumerable<Reserva> ObtenerReservasPorAsignatura(string asignatura)
+        {
+            if (string.IsNullOrWhiteSpace(asignatura))
+                throw new ArgumentException("La asignatura no puede estar vacía.", nameof(asignatura));
+
+            return _reservaRepository.ObtenerPorAsignatura(asignatura);
+        }
+        public IEnumerable<Reserva> ObtenerTodasLasReservas()
+        {
+            return _reservaRepository.ObtenerTodos();
+        }
+        public void EliminarReserva(int id)
+        {
+            var reserva = _reservaRepository.ObtenerPorId(id);
+            if (reserva == null)
+                throw new KeyNotFoundException($"No se encontró una reserva con el ID {id}.");
+
+            _reservaRepository.Eliminar(reserva);
+        }
+
+        // Método para actualizar una reserva
+        public void ActualizarReserva(Reserva reserva)
+        {
+            if (reserva == null)
+                throw new ArgumentNullException(nameof(reserva), "La reserva no puede ser nula.");
+
+            _reservaRepository.Actualizar(reserva);
+        }
     }
 }
